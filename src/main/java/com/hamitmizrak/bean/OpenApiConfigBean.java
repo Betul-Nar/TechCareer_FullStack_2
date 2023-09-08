e4d5be7281acfe52eefe83005f2017995c23a465
betül nar <betnarr@hotmail.com>
2023-09-08 17:40:28 +0300

    tüm bean yapilarii

---
+++ src/main/java/com/hamitmizrak/bean/OpenApiConfigBean.java
@@ -0,0 +1,26 @@
+package com.hamitmizrak.bean;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfigBean {
+
+    @Bean
+    public OpenAPI openAPIMethod(){
+        return new OpenAPI()
+                .info(//bilgilendirme için kullanıyoruz
+                        new Info()
+                                .description("blog tanımlama")
+                                .version("v1.0")
+                                .contact(new Contact().email("hamitmizrak@gmail.com").url("url"))
+                                .title("title")
+                                .termsOfService("Software INC")
+                                .license(new License().url("http://www.").name("name")));//
+    }
+}
